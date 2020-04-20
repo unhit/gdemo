03021e9ab869d98c1297284b60bb35aced81d676
Tomasz Kruk <unhitpl@gmail.com>
2020-04-20 16:52:15 +0200

    Update urt version to 4.3.4

--- variable.h
+++ variable.h
@@ -4,7 +4,7 @@
 // entityState_t->eType values
 
 // 4.2.023
-#define GAME_VER                "4.2.023"
+#define GAME_VER                "4.3.4"
 #define GD_ET_0x01		0x01
 #define GD_ET_HIT               0x51
 #define GD_ET_MISS_HIT          0x52
